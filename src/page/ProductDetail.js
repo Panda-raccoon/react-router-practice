22deeb11e0b3639f936bdb672c9f84226e80fcc9
Panda-racoon <millie3519@gmail.com>
2025-10-30 03:19:51 +0900

    상품상세페이지 디자인

--- src/page/ProductDetail.js
+++ src/page/ProductDetail.js
@@ -1,7 +1,46 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import { Container, Row, Col, Button } from "react-bootstrap";
+import { useParams } from "react-router-dom";
 
 const ProductDetail = () => {
-  return <div>상품상세페이지</div>;
+  let { id } = useParams();
+  const [product, setProduct] = useState(null);
+  const getProductDetail = async () => {
+    let url = `http://localhost:5000/products/${id}`;
+    let response = await fetch(url);
+    let data = await response.json();
+    setProduct(data);
+  };
+  useEffect(() => {
+    getProductDetail();
+  }, []);
+
+  return (
+    <Container className="product-detail-container">
+      <Row>
+        <Col md={6}>
+          <img
+            src={product?.img}
+            alt="Product"
+            className="product-detail-image"
+          />
+        </Col>
+        <Col md={6} className="product-detail-info">
+          <h2>{product?.title}</h2>
+          <h3>₩{product?.price}</h3>
+          <select className="size-dropdown">
+            {product?.size.map((size) => (
+              <option key={size} value={size}>
+                {size}
+              </option>
+            ))}
+          </select>
+
+          <Button variant="dark">구매하기</Button>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default ProductDetail;
